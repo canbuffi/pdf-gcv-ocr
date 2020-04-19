e8ef8198688fb4a66b67fa82bf10d939da7aeb86
qnstie <pawel@kunstman.net>
2020-04-19 21:20:33 +0200

    Corrected handling of spaces in directory and file names.

--- makePDF.sh
+++ makePDF.sh
@@ -1,7 +1,7 @@
 if [[ -d "$1" ]]
 then
-	echo ./lib/hocr-pdf.py $1
-    ./lib/hocr-pdf.py $1 > $1.new.pdf
+	echo ./lib/hocr-pdf.py "$1"
+    ./lib/hocr-pdf.py "$1" > "$1".new.pdf
 else
     echo "Provided argument is not a directory"
 fi
